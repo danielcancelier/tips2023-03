1ba0a9421c3a8cca1239ed8f160c94f5ce5c2c51
EAT-Curitiba <66984602+danielcancelier@users.noreply.github.com>
2023-03-24 01:29:02 -0300

    segundo

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
 import streamlit as st
 import pandas as pd
-from PIL import Image
+import base64
 
-# Define o nome e os campos do arquivo CSV
-filename = "tips_2023_03.csv"
-fields = ["nome", "data_reserva", "data_pagto"]
 
 st.markdown('''
-## 1a Etapa TIPS - 6a TEMPORADA
+## TIPS
+1a Etapa - 6a TEMPORADA
 :spades: :hearts: :diamonds: :clubs:
 
-## Dia 28/03/2023 - TERÇA-FEIRA - às 19 h na P2W Setor Hoteleiro Norte
+Dia 28/03/2023 - TERÇA-FEIRA - às 19 h na P2W Setor Hoteleiro Norte
 
 - Buy-in antecipado R$ 70,00 (1K extra em fichas)
 - PIX tips2023
@@ -19,6 +17,10 @@ st.markdown('''
 ''')
 image = Image.open('tips.jpg')
 st.image(image, caption='TIPS 2015')
+
+# Define o nome e os campos do arquivo CSV
+filename = "tips_2023_03.csv"
+fields = ["nome", "data_reserva", "data_pagto"]
 
 # Cria o arquivo CSV caso ainda não exista
 try:
@@ -50,5 +52,21 @@ if st.button("Excluir"):
     st.write("Participante excluído com sucesso!")
     st.write(df)
 
+# Inclui a data de pagamento de um participante
+st.write("Incluir data de pagamento:")
+pay_name = st.selectbox("Selecione o participante para incluir a data de pagamento", options=df["nome"])
+pay_date = st.date_input("Selecione a data de pagamento", value=pd.Timestamp.now().date())
+if st.button("Salvar"):
+    df.loc[df["nome"] == pay_name, "data_pagto"] = pay_date.strftime("%Y-%m-%d")
+    st.write("Data de pagamento incluída com sucesso!")
+    st.write(df)
+
 # Salva os dados no arquivo CSV
 df.to_csv(filename, index=False)
+
+# Cria um link para fazer download do arquivo CSV
+csv = df.to_csv(index=False)
+b64 = base64.b64encode(csv.encode()).decode()
+st.markdown(f"### Download do arquivo CSV")
+href = f'<a href="data:file/csv;base64,{b64}" download="{filename}">Clique aqui para fazer download do arquivo CSV</a>'
+st.markdown(href, unsafe_allow_html=True)
